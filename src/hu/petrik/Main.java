b848368ce5f98fd06582c64d7f9d101f7b0c0cb4
kekesibence <bence.kekesi@gmail.com>
2021-11-10 16:31:06 +0100

    bus feladat elkészítése

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,8 +1,12 @@
 package hu.petrik;
 
+import hu.petrik.Exercise.Bus;
+import hu.petrik.Exercise.SzobaKerekpar;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        SzobaKerekpar.Run();
+        Bus.Run();
     }
 }
